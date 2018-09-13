b360d1217d9d2a8a69f7814d6bf836275aacd9b2
atam84 <amine.tamdy@gmail.com>
2018-09-13 21:12:27 +0100

    restart

--- imports/ui/components/env/env.js
+++ imports/ui/components/env/env.js
@@ -21,73 +21,101 @@ Template.updateEnv.helpers({
     'envCollection': () => {
         return _collection;
     },
-    'selectedEnv': () => {
-        return _collection.findOne({_id: session.get('selectedEnv')});
-    },
     'selectedDocument': () => {
-        return _collection.findOne({_id: Session.get('selectedDocument')._id});
+        return getDocumentById(_collection, getSelectedDocumentId());
+        //return _collection.findOne({_id: Session.get('selectedDocument')._id});
     }
 });
 
 Template.detailsEnv.helpers({
     'selectedDocument': () => {
-        return _collection.findOne({_id: FlowRouter.getParam('_id')});
+        return getDocumentById(_collection, FlowRouter.getParam('_id'));
+        //return _collection.findOne({_id: FlowRouter.getParam('_id')});
     },
 });
 
 Template.detailsEnv.events({
     'click .mod-env-modal': (e) => {
-        Session.set('inModalTemplete', {
+        setInModalTemplate({
             template: 'updateEnv',
             label: 'Update environnement'
         });
-        Session.set('selectedDocument', {
-            _id: e.target.id,
-        });
-        console.dir(Session.get('selectedDocument'));
+        setSelectedDocument(e.target.id);
     },
 });
 
-Template.envs.helpers({
+Template.Envs.helpers({
     'envCollection': () => {
-        console.log('envCollection called');
-        _res = _collection.find({}).fetch();
-        //console.dir(_res);
-        return _res;
+        return loadDocuments(_collection);
     },
 });
 
-Template.envs.events({
+Template.Envs.events({
     'click .btn-remove': (e) => {
-        console.log(e.target.id);
-        _collection.remove({_id: e.target.id});
+        //_collection.remove({_id: e.target.id});
+        removeDocument(_collection, e.target.id);
     },
     'click .env-modal': (e) => {
-        Session.set('inModalTemplete', {
+        setInModalTemplate({
             template: 'insertEnv',
             label: 'Add new environnement'
         });
-        console.log(e.target);
+        resetSelectedDocument();
     },
     'click .mod-env-modal': (e) => {
-        Session.set('inModalTemplete', {
+        setInModalTemplate({
             template: 'updateEnv',
             label: 'Update environnement'
         });
-        Session.set('selectedDocument', {
-            _id: e.target.id,
-        });
-        console.dir(Session.get('selectedDocument'));
+        setSelectedDocument(e.target.id);
     },
     'click .details-env-modal': (e) => {
-        Session.set('inModalTemplete', {});
-        Session.set('selectedDocument', {
-            _id: e.target.id,
-        });
-        console.dir(Session.get('selectedDocument'));
+        resetInModalTemplate();
+        setSelectedDocument(e.target.id);
     }
 });
 
 
+const loadDocuments = (MongoCollection) => {
+    return MongoCollection.find({}).fetch();
+}
 
+const removeDocument = (MongoCollection, docId) => {
+    MongoCollection.remove({_id: docId}, (err) => {
+        if(err) {
+            console.log('Error: ' + err);
+        }
+    });
+}
+
+const resetInModalTemplate = () => {
+    Session.set('inModalTemplate', {});
+}
+
+const resetSelectedDocument = () => {
+    Session.set('selectedDocument', {});
+}
+
+const setSelectedDocument = (docId) => {
+    Session.set('selectedDocument', {
+        _id: docId,
+    });
+}
+
+const getSelectedDocumentId = () => {
+    let _selected = Session.get('selectedDocument');
+    if (_selected !== undefined) {
+        return docId = _selected._id ? _selected.id : undefined;
+    } else {
+        return undefined;
+    }
+}
+
+const setInModalTemplate = (settings) => {
+    Session.set('inModalTemplete', settings);
+}
+
+const getDocumentById = (MongoCollection, docId) => {
+    return MongoCollection.findOne({_id: docId});
+}
 
